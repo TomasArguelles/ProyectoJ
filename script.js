ff5658e16c5bd729bb597d428f801d572fcb9859
Tomás Argüelles <47278751+TomasArguelles@users.noreply.github.com>
2025-12-11 21:36:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,35 @@ const screens = [
   {
     title: "La casa de las bestias",
     text: "En la pequeña guarida de las bestias, las estrellas te mostrarán el camino",
-    image: "imagenes/pensando.webp",
+    image: "imagenes/pensando.jpg",
     pass: "cabañaDeLosPerros",
-    hint: "imagenes/hint1.webp"
+    hint: "imagenes/hint1.jpg"
   },
   {
     title: "Nectar de los dioses",
     text: "Ambrosía, nectar y manjar, nada se puede hacer sin agitar",
-    image: "imagenes/Wiii.webp",
+    image: "imagenes/Wiii.jpg",
     pass: "CocteleraDeSalón",
-    hint: "imagenes/hint2.webp"
+    hint: "imagenes/hint2.jpg"
   },
   {
     title: "Las cartas están echadas",
     text: "Has llegado hasta aquí sin apostar, pero a partir de ahora tus cartas debes jugar",
-    image: "imagenes/Woha.webp",
+    image: "imagenes/Woha.jpg",
     pass: "CasinoPortatil100",
-    hint: "imagenes/hint3.webp"
+    hint: "imagenes/hint3.jpg"
   },
   {
     title: "Solo respuestas contundentes",
     text: "Cada problema requiere una solución, y cada solución una herramienta",
-    image: "imagenes/Wishh.webp",
+    image: "imagenes/Wishh.jpg",
     pass: "MartilloDeMadera!",
-    hint: "imagenes/hint4.webp"
+    hint: "imagenes/hint4.jpg"
   },
   {
     title: "Fin",
     text: "Gracias por participar en la Jincana amorcito, espero que te haya gustado y no haya sido demasiado facil/dificil (dificil de saber pues estoy escribiendo este codigo con antelación xD). Esto es una pseudo-carta, una pseudo-recompensa, y viene con pseudo-besitos. Lo cierto es que esta pequeña carta ha sido escrita a lo largo de la quedada, al principio pensaba esconderlo tod en papel, pero acabé pasandolo a programa porque recordé que, hace tiempo, te había parecido muy chulo cuando un chico le había programado un regalo a su pareja en las redes, así que hice mi propia versión, si bien no será tan chula como me gustaría, solo espero que sea algo que te haga ilusión y te guste, porque no hay nada más bello en tí que la felicidad que muestras cuando te brillan los ojos y deslumbra tu sonrisa. Espero que esta quedada haya sido especial para ti, aal igual que lo ha sido para mi, y que aunque no hayamos podido ir al zoo esta vez, recuerdes que todos, y sobretodo yo (;3), te queremos mucho. Felicidades amorcito, te quiero, feliz cumpleaños",
-    image: "imagenes/fin.webp",
+    image: "imagenes/fin.jpg",
     pass: null,
     hint: null
   }
